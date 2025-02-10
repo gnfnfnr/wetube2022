214b473ae8054d26db971943b773348b5d47f70c
ramda <91455804+gnfnfnr@users.noreply.github.com>
2025-02-10 14:18:49 +0900

    learn: how to mixin pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,8 +3,15 @@ const fakeuser = {
   loggedIn: true,
 };
 
-export const trending = (req, res) =>
-  res.render("home", { pagetitle: "홈화면", fakeuser });
+export const trending = (req, res) => {
+  const videos = [
+    { title: "first video", ratting: 5, comment: 2, views: 59 },
+    { title: "second video", ratting: 5, comment: 2, views: 59 },
+    { title: "third video", ratting: 5, comment: 2, views: 59 },
+    { title: "gg" },
+  ];
+  res.render("home", { pagetitle: "홈화면", fakeuser, videos });
+};
 export const see = (req, res) => res.send("비디오 홈페이지 watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("비디오 홈페이지 search");
