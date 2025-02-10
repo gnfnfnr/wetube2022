04da4ebb24f1ca9b732912ead4d7052d07e54a96
ramda <91455804+gnfnfnr@users.noreply.github.com>
2025-02-10 12:13:47 +0900

    feat: learn how to use conditionals in pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,10 @@
-export const trending = (req, res) => res.render("home");
+const fakeuser = {
+  username: "lucy",
+  loggedIn: true,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pagetitle: "홈화면", fakeuser });
 export const see = (req, res) => res.send("비디오 홈페이지 watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("비디오 홈페이지 search");
